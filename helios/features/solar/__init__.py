c4da3da9d74164480b1d7f2494d0e2cd732cc161
aditya622600-maker <aditya622600@gmail.com>
2026-08-22 15:35:01 +0530

    feat(p3): expose solar yield interface

---
+++ helios/features/solar/__init__.py
@@ -0,0 +1,3 @@
+from .yield_model import SolarAssumptions, SolarFeature, SolarResource, calculate_solar_feature
+
+__all__ = ["SolarAssumptions", "SolarFeature", "SolarResource", "calculate_solar_feature"]
